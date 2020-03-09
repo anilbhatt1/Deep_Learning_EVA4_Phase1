b67bcbe53db52642d82779355bfbb0f539041474
Anilkumar N Bhatt <43835604+anilbhatt1@users.noreply.github.com>
2020-03-09 18:38:32 +0530

    Update S7_Dataloaders.py

--- S7_CIFAR10_Modular/models/S7_Dataloaders.py
+++ S7_CIFAR10_Modular/models/S7_Dataloaders.py
@@ -6,9 +6,9 @@ import torch
 class DataLoader:
       
     def __init__(self, shuffle, batch_size, seed):
-        self.shuffle = True,
-        self.batch_size = 128,
-        self.seed    = 1
+        self.shuffle = shuffle,
+        self.batch_size = batch_size,
+        self.seed    = seed
          
         cuda = torch.cuda.is_available()
             
@@ -16,7 +16,7 @@ class DataLoader:
            torch.cuda.manual_seed(seed) # Seed is for reproducibility
       
         # # dataloader arguments # # which we load
-        self.dataloader_args = dict(shuffle=shuffle, batch_size=batch_size, num_workers=4, pin_memory=True) if cuda else dict(shuffle=shuffle, batch_size=batch_size)
+        self.dataloader_args = dict(shuffle=shuffle, batch_size=batch_size, num_workers=4, pin_memory=True) if cuda else dict(shuffle=shuffle, batch_size=batch_size/2)
 
         def load(self, data):
             return torch.utils.data.DataLoader(data, **self.dataloader_args)
