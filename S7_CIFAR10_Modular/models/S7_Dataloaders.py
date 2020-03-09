e609bd1f6390fd5401c724203223a2580c320b1a
Anilkumar N Bhatt <43835604+anilbhatt1@users.noreply.github.com>
2020-03-09 09:40:05 +0530

    Create S7_Dataloaders.py

---
+++ S7_CIFAR10_Modular/models/S7_Dataloaders.py
@@ -0,0 +1,22 @@
+# Import the torch library
+import torch 
+
+# Class to create a Data Loader 
+# Data Loader is a place holder using which we will load the train and test data sets.
+Class DataLoader:
+      
+    def __init__(self, shuffle, batch_size, seed):
+        self.shuffle = True,
+        self.batch_size = 128,
+        self.seed    = 1
+         
+        cuda = torch.cuda.is_available()
+            
+        if cuda:
+           torch.cuda.manual_seed(seed) # Seed is for reproducibility
+      
+        # # dataloader arguments # # which we load
+        self.dataloader_args = dict(shuffle=shuffle, batch_size=batch_size, num_workers=4, pin_memory=True) if cuda else dict(shuffle=shuffle, batch_size=batch_size)
+
+        def load(self, data):
+            return torch.utils.data.DataLoader(data, **self.dataloader_args)
