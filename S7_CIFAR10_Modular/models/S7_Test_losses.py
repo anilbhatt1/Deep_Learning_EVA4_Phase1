5982ba7518b4f5d0f72ebc3b1c9c4db9f734af14
Anilkumar N Bhatt <43835604+anilbhatt1@users.noreply.github.com>
2020-03-11 16:37:30 +0530

    Update S7_Test_losses.py

--- S7_CIFAR10_Modular/models/S7_Test_losses.py
+++ S7_CIFAR10_Modular/models/S7_Test_losses.py
@@ -16,14 +16,12 @@ class Test_loss:
        
            model.eval()
            
-           correct        = 0
-           processed      = 0    
+           correct        = 0 
+           total          = 0              
            test_loss      = 0
            test_accuracy  = 0 
            test_losses    = []
            test_acc       = []
-           test_new       = 0
-           total          = 0
            
            with torch.no_grad():               # For test data, we won't do backprop, hence no need to capture gradients
                 for images,labels in test_loader:
@@ -35,19 +33,12 @@ class Test_loss:
                     correct          += labels_pred_max.eq(labels.view_as(labels_pred_max)).sum().item()
                     total            += labels.size(0) 
                 
-                processed   = len(test_loader)
-                print('processed',processed)
-                print('correct',correct)
-                print('total',total)
-                test_loss   /= processed  # Calculating overall test loss for the epoch
+                test_loss   /= total  # Calculating overall test loss for the epoch
                 test_losses.append(test_loss)    
                                   
-                test_accuracy =  correct/processed
-                test_acc.append(test_accuracy)   
-                
-                test_new = correct/total
-                  
-                print('\nTest set: Average loss: {:.4f}, Test Accuracy: {:.2f}, Test new: {:.2f}\n'
-                      .format(test_loss, test_accuracy,test_new))
+                test_accuracy =  correct/total
+                test_acc.append(test_accuracy)             
+               
+                print('\nTest set: Average loss: {:.4f}, Test Accuracy: {:.2f}\n' .format(test_loss, test_accuracy,test_new))
 
            return test_losses, test_acc
