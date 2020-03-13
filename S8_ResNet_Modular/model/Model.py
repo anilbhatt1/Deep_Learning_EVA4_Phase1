67cc0ab8dae1f19f48fed6b347b56d2bf9e68456
Anilkumar N Bhatt <43835604+anilbhatt1@users.noreply.github.com>
2020-03-13 19:29:13 +0530

    Update Model.py

--- S8_ResNet_Modular/model/Model.py
+++ S8_ResNet_Modular/model/Model.py
@@ -3,83 +3,62 @@ import torch.nn as nn
 import torch.nn.functional as F
 dropout_value = 0.05
 
-class Net(nn.Module):
+class BasicBlock(nn.Module):
+    expansion = 1
 
-    def __init__(self):
-        super(Net, self).__init__()
-        # CONVOLUTION BLOCK 1
-        self.convblock1A = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3, 3), padding=1, bias=False),
-            nn.ReLU(),
-            nn.BatchNorm2d(32),
-            nn.Dropout(dropout_value)
-        ) # in = 32x32x3 , out = 32x32x32, RF = 3
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
 
-        self.dilated1B = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=2, bias=False, dilation=2),
-            nn.ReLU(),
-            nn.BatchNorm2d(64),
-            nn.Dropout(dropout_value)
-        ) # in = 32x32x32 , out = 32x32x64, RF = 7
- 
-        # TRANSITION BLOCK 1
-        self.pool1 = nn.MaxPool2d(2, 2) # in = 32x32x64 , out = 16x16x64, RF = 8
-        self.tran1 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=(1, 1), padding=0, bias=False)
-        ) # in = 16x16x64 , out = 16x16x32, RF = 6        
-
-        # CONVOLUTION BLOCK 2
-        self.convblock2A = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=1, bias=False),
-            nn.ReLU(),
-            nn.BatchNorm2d(64),
-            nn.Dropout(dropout_value)
-        ) # in = 16x16x32 , out = 16x16x64, RF = 12
-        self.depthwise2B = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), padding=1, bias=False, groups=64),
-            nn.ReLU(),
-            nn.BatchNorm2d(64),
-            nn.Dropout(dropout_value)   
-        ) # in = 16x16x1x64 , out = 16x16x64, RF = 16
-        self.pointwise2C = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(1, 1), padding=0, bias=False)
-        ) # in = 16x16x64 , out = 16x16x128, RF = 16   
-
-        # TRANSITION BLOCK 2
-        self.pool2 = nn.MaxPool2d(2, 2) # in = 16x16x128 , out = 8x8x128, RF = 18
-        self.tran2 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=32, kernel_size=(1, 1), padding=0, bias=False)
-        ) # in = 8x8x128 , out = 8x8x32, RF = 18        
-
-        # CONVOLUTION BLOCK 3
-        self.convblock3A = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=1, bias=False),
-            nn.ReLU(),
-            nn.BatchNorm2d(64),
-            nn.Dropout(dropout_value)
-        ) # in = 8x8x32 , out = 8x8x64, RF = 26
-        
-        # TRANSITION BLOCK 3
-        self.pool3 = nn.MaxPool2d(2, 2) # in = 8x8x64 , out = 4x4x64, RF = 30
-        self.tran3 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=(1, 1), padding=1, bias=False)
-        ) # in = 4x4x64 , out = 4x4x32, RF = 30
-        
-        # OUTPUT BLOCK
-        self.Gap1 = nn.Sequential(
-            nn.AvgPool2d(kernel_size=4)
-        ) # in = 4x4x32 , out = 1x1x32, RF = 54	
-        self.fc1 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=10, kernel_size=(1, 1), padding=0, bias=False)
-        ) # in = 1x1x32 , out = 1x1x10, RF = 54
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
 
     def forward(self, x):
-        x = self.dilated1B(self.convblock1A(x))
-        x = self.tran1(self.pool1(x))
-        x = self.pointwise2C(self.depthwise2B(self.convblock2A(x)))
-        x = self.tran2(self.pool2(x))
-        x = self.convblock3A(x)
-        x = self.tran3(self.pool3(x))
-        x = self.fc1(self.Gap1(x))
-        x = x.view(-1, 10)
-        return F.log_softmax(x, dim=-1)
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+    
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def ResNet18():
+    return ResNet(BasicBlock, [2,2,2,2])
