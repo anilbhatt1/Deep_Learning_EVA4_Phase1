751c0c45150cea7832fbaa56b13ef27ff93760ac
Anilkumar N Bhatt <43835604+anilbhatt1@users.noreply.github.com>
2020-03-21 09:31:10 +0530

    Update Alb_data_transforms.py

--- S9_Resnet_Gradcam_Albumentations/models/Alb_data_transforms.py
+++ S9_Resnet_Gradcam_Albumentations/models/Alb_data_transforms.py
@@ -1,39 +1,25 @@
 from torchvision import transforms
+import albumentations as A
+import albumentations.pytorch as AP
+import random
+import numpy as np
 
-# # class for Transformations ## 
-class Transforms_custom:
-      def __init__(self,normalize=False, mean=None, stdev=None):
-      
-          self.normalize = normalize
-          self.mean      = mean      ## Make sure you pass the meand and stdev whenever normalization is set to true 
-          self.stdev     = stdev
-      
-      
-      # Define a method for test data set as it does not need extra transformations.
-      # We just need to normalize with mean and stdev
-      
-      def test_transforms(self):
-          transforms_list = [transforms.ToTensor()]
-          if (self.normalize):
-             transforms_list.append(transforms.Normalize(self.mean,self.stdev))
-          return transforms.Compose(transforms_list)
-          
-      # Define a method for train data . It can have multiple transformations other than changing to tensor and normalizing
-      # so create your lists accordingly . One for before converting to tensor and normalization 
-      # one for after converting it to tensor and normalization
-      
-      
-      def train_transforms(self , before_norm=None, after_norm=None):
-          if before_norm:
-             transforms_list = before_norm 
-             transforms_list.append(transforms.ToTensor())
-          else:
-             transforms_list = [transforms.ToTensor()]
-             
-          if (self.normalize):
-             transforms_list.append(transforms.Normalize(self.mean,self.stdev))
-           
-          if after_norm:
-             transforms_list.extend(after_norm)
-           
-          return transforms.Compose(transforms_list)
+
+class Albumentations_transform:
+  """
+  Class to create test and train transforms using Albumentations. ToTensor() will be appended at the end of transforms list
+  """
+  def __init__(self, transforms_list=[]):
+    print('__transformation__')
+    transforms_list.append(AP.ToTensor())
+    
+    self.transforms = A.Compose(transforms_list)
+
+  """
+  Wrapper Model to convert to np array
+  """
+  def __call__(self, img):
+    print('__call_')  
+    img = np.array(img)
+    #print(img)
+    return self.transforms(image=img)['image']
