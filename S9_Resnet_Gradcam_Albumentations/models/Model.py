96bca7d20125b6598c4bbc9ee69efc2e05b5eba2
Anilkumar N Bhatt <43835604+anilbhatt1@users.noreply.github.com>
2020-03-21 23:54:30 +0530

    Update Model.py

--- S9_Resnet_Gradcam_Albumentations/models/Model.py
+++ S9_Resnet_Gradcam_Albumentations/models/Model.py
@@ -165,34 +165,34 @@ class CIFAR10Net_S9(nn.Module):
             nn.ReLU(),
             nn.BatchNorm2d(64),
             nn.Dropout(dropout_value)
-        ) # in = 32x32x32 , out = 32x32x64, RF = 7
+        ) # in = 32x32x32 , out = 32x32x64, RF = 5
  
         # TRANSITION BLOCK 1
-        self.pool4 = nn.MaxPool2d(2, 2) # in = 32x32x64 , out = 16x16x64, RF = 8
+        self.pool4 = nn.MaxPool2d(2, 2) # in = 32x32x64 , out = 16x16x64, RF = 6
 
         self.convblock5 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), padding=1, bias=False),
             nn.ReLU(),
             nn.BatchNorm2d(64),
             nn.Dropout(dropout_value)
-        ) # in = 16x16x32 , out = 16x16x64, RF = 12
+        ) # in = 16x16x64 , out = 16x16x64, RF = 10
         
         self.convblock6 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), padding=1, bias=False),
             nn.ReLU(),
             nn.BatchNorm2d(64),
             nn.Dropout(dropout_value)   
-        ) # in = 16x16x1x64 , out = 16x16x64, RF = 16
+        ) # in = 16x16x1x64 , out = 16x16x64, RF = 14
         
         self.convblock7 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), padding=1, bias=False),
             nn.ReLU(),
             nn.BatchNorm2d(64),
             nn.Dropout(dropout_value)   
-        ) # in = 16x16x1x64 , out = 16x16x64, RF = 16 
+        ) # in = 16x16x1x64 , out = 16x16x64, RF = 18 
 
         # TRANSITION BLOCK 2
-        self.pool8 = nn.MaxPool2d(2, 2) # in = 32x32x64 , out = 16x16x64, RF = 8      
+        self.pool8 = nn.MaxPool2d(2, 2) # in = 16x16x64 , out = 8x8x64, RF = 20
     
 
         # CONVOLUTION BLOCK 3
@@ -201,29 +201,29 @@ class CIFAR10Net_S9(nn.Module):
             nn.ReLU(),
             nn.BatchNorm2d(64),
             nn.Dropout(dropout_value)
-        ) # in = 8x8x32 , out = 8x8x64, RF = 26
+        ) # in = 8x8x64 , out = 8x8x32, RF = 28
         
         self.convblock10 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), padding=1, bias=False),
             nn.ReLU(),
             nn.BatchNorm2d(64),
             nn.Dropout(dropout_value)
-        ) # in = 8x8x32 , out = 8x8x64, RF = 26 
+        ) # in = 8x8x64 , out = 8x8x64, RF = 36 
         
         self.convblock11 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=32, kernel_size=(3, 3), padding=1, bias=False),
             nn.ReLU(),
             nn.BatchNorm2d(32),
             nn.Dropout(dropout_value)
-        ) # in = 8x8x32 , out = 8x8x64, RF = 26              
+        ) # in = 8x8x64 , out = 8x8x32, RF = 44              
       
         # OUTPUT BLOCK
         self.Gap1 = nn.Sequential(
             nn.AvgPool2d(kernel_size=8)
-        ) # in = 4x4x32 , out = 1x1x32, RF = 54	
+        ) # in = 8x8x32 , out = 1x1x32, RF = 72	
         self.fc1 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=10, kernel_size=(1, 1), padding=0, bias=False)
-        ) # in = 1x1x32 , out = 1x1x10, RF = 54
+        ) # in = 1x1x32 , out = 1x1x10, RF = 72
 
     def forward(self, x):
         x = self.convblock3(self.convblock2(x))
