3323fb3fd6d0f800388b4af6fc4b9f619b7721de
Anilkumar N Bhatt <43835604+anilbhatt1@users.noreply.github.com>
2020-03-28 08:25:35 +0530

    Update Test_losses.py

--- S10_LRFinder_ReduceLRonPlateau/models/Test_losses.py
+++ S10_LRFinder_ReduceLRonPlateau/models/Test_losses.py
@@ -28,7 +28,7 @@ class Test_loss:
            actual_class   = []
            wrong_predict  = []
            count_wrong    = 0 
-           classwise_acc  = []
+           
            label_dict     = {0:0, 1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9}
            label_total    = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
            label_correct  = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}   
@@ -56,14 +56,14 @@ class Test_loss:
                               predicted_class.append(labels_pred_max[i].item())
                               actual_class.append(labels[i].item())
                               count_wrong += 1
-                              print('count_wrong:',count_wrong)
-                              print('labels_pred_max[i].shape & type :', labels_pred_max[i], type(labels_pred_max[i]))
-                              print('labels_pred_max[i] :', labels_pred_max[i])
-                              print('labels[i].shape :', labels[i].shape)
-                              print('labels[i]:', labels[i])     
-                              x1 = labels[i].item()
-                              print('x1:',x1)
-                              print('type(x1):',type(x1))     
+                              #print('count_wrong:',count_wrong)
+                              #print('labels_pred_max[i].shape & type :', labels_pred_max[i], type(labels_pred_max[i]))
+                              #print('labels_pred_max[i] :', labels_pred_max[i])
+                              #print('labels[i].shape :', labels[i].shape)
+                              #print('labels[i]:', labels[i])     
+                              #x1 = labels[i].item()
+                              #print('x1:',x1)
+                              #print('type(x1):',type(x1))     
                 
                 test_loss   /= total  # Calculating overall test loss for the epoch
                 test_losses.append(test_loss)    
